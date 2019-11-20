571bf1fe55b49ec86000cae9d12917ddb72126a5
Manuel A. Vázquez <manuavazquez@gmail.com>
2019-11-20 17:27:03 +0100

    - added .gitignore - added .idea - added parameters.yaml => - scraper.py 	- ...most parameters are read from there - downloader.py -> download.py 	- new function "files_list"

---
+++ download.py
@@ -0,0 +1,83 @@
+import pathlib
+import requests
+import urllib.parse
+from typing import Union, Optional, Collection, List
+
+current_dir = pathlib.Path.cwd()
+
+
+def file(url: str, output_file: Union[str, pathlib.Path]) -> None:
+	"""
+	Download a file from an URL
+
+	Parameters
+	----------
+	url: str
+		The URL.
+	output_file: str or pathlib object
+		The path to the output file that will be created.
+
+	"""
+
+	read_url = requests.get(url)
+
+	with open(output_file, 'wb') as f:
+
+		f.write(read_url.content)
+
+
+def files_list(
+		files_relative_paths: Collection[str], downloads_directory: Union[str, pathlib.Path], homepage: str,
+		sizes: Optional[Collection[str]] = None) -> List[pathlib.Path]:
+	"""
+	Downloads the URLs.
+
+	Parameters
+	----------
+	files_relative_paths: iterable over str
+		Sequence of *relative* paths to files.
+	downloads_directory: str or pathlib object
+		Directory where the files will be downloaded.
+	homepage: str
+		Base URL with respect to which the `relative_paths` are specified.
+	sizes: iterable over str, optional
+		Sizes of the files.
+
+	Returns
+	-------
+	downloaded_files: list of pathlib.Path
+		The paths of the downloaded files.
+
+	"""
+
+	# if a `Collection` of sizes was passed...
+	if sizes is not None:
+
+		# ...we make sure there is one size per file
+		assert len(files_relative_paths) ==  len(sizes)
+
+	# if no `Collection` of sizes was passed...
+	else:
+
+		# ...a generator of empty strings is used instead
+		sizes = ("" for _ in range(len(files_relative_paths)))
+
+	output_dir = current_dir / downloads_directory
+
+	output_dir.mkdir(parents=True, exist_ok=True)
+
+	# a list with the files downloaded
+	downloaded_files = []
+
+	for file, size in zip(files_relative_paths, sizes):
+
+		output_file = output_dir / pathlib.Path(file).name
+
+		print(f'downloading "{output_file.relative_to(current_dir)}" {size}')
+
+		# file(urllib.parse.urljoin(homepage, file), output_file)
+
+		# the output file path is added to the list of downloaded files
+		downloaded_files.append(output_file)
+
+	return downloaded_files
--- scraper.py
+++ scraper.py
@@ -3,38 +3,55 @@
 import pathlib
 import re
 import urllib.parse
+import zipfile
+
+import yaml
 
 import parser
-import downloader
+import download
 
-nih_homepage = 'https://exporter.nih.gov'
+# ==================== parameters reading
+
+# parameters are read
+with open('parameters.yaml') as yaml_data:
+
+	# the parameters file is read to memory
+	parameters = yaml.load(yaml_data, Loader=yaml.FullLoader)
+
+# ---
+
+nih_homepage = parameters['input']['NIH']['homepage']
+
+projects_relative_path = parameters['input']['NIH']['projects']['relative path']
+projects_n_table = parameters['input']['NIH']['projects']['table number']
+projects_links_column = parameters['input']['NIH']['projects']['links column']
+
+publications_relative_path = parameters['input']['NIH']['publications']['relative path']
+publications_n_table = parameters['input']['NIH']['publications']['table number']
+publications_links_column = parameters['input']['NIH']['publications']['links column']
+
+downloads_directory = pathlib.Path(parameters['output']['downloads directory'])
+
+# ====================
 
 current_dir = pathlib.Path.cwd()
 
-projects_url = urllib.parse.urljoin(nih_homepage, 'ExPORTER_Catalog.aspx?sid=1&index=0')
-publications_url = urllib.parse.urljoin(nih_homepage, 'ExPORTER_Catalog.aspx?sid=0&index=2')
-
-output_dir = current_dir / 'publications'
+projects_url = urllib.parse.urljoin(nih_homepage, projects_relative_path)
+publications_url = urllib.parse.urljoin(nih_homepage, publications_relative_path)
 
 re_javascript = re.compile('^javascript.*')
 
 # patterns_to_be_dropped = [re_javascript, re.compile('.*_DUNS_.*\.zip$'), re.compile('.*_PRJFUNDING_.*\.zip$')]
-# df = parser.parse_table(projects_url, 12, patterns_to_be_dropped)
+# df = parser.parse_table(projects_url, projects_n_table, patterns_to_be_dropped)
 
 patterns_to_be_dropped = [re_javascript, re.compile('.*_AFFLNK_.*\.zip$')]
 
-df = parser.parse_table(publications_url, 12, patterns_to_be_dropped)
+df = parser.parse_table(publications_url, publications_n_table, patterns_to_be_dropped)
 
 # =================================== downloading
 
-output_dir.mkdir(parents=True, exist_ok=True)
+downloaded_files = download.files_list(df[publications_links_column], downloads_directory, nih_homepage, df['CSV'])
 
-for _, row in df.iterrows():
-
-	relative_url = row['CSV_link']
-
-	output_file = output_dir / pathlib.Path(row['CSV_link']).name
-
-	print(f'downloading "{output_file.relative_to(current_dir)}" {row["CSV"]}')
-
-	downloader.file(urllib.parse.urljoin(nih_homepage, row['CSV_link']), output_file)
+# with zipfile.ZipFile(downloaded_files[0]) as downloaded_zip:
+#
+# 	downloaded_zip.extractall(path=downloads_directory / 'unzipped')
