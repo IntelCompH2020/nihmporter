e1306d332af2f0db2a94b10b1042199c5ce06560
Manuel A. Vázquez <manuavazquez@gmail.com>
2019-11-29 09:18:26 +0100

    - simplified the code

--- scraper.py
+++ scraper.py
@@ -22,61 +22,38 @@ with open('parameters.yaml') as yaml_data:
 # ---
 
 # common parameters
-nih_homepage = parameters['input']['NIH']['homepage']
+nih_homepage = parameters['homepage']
 downloads_directory = pathlib.Path(parameters['output']['downloads directory'])
 unzipped_files_directory = pathlib.Path(parameters['output']['unzipped files directory'])
 
-# projects
-projects_relative_path = parameters['input']['NIH']['projects']['relative path']
-projects_n_table = parameters['input']['NIH']['projects']['table number']
-projects_feather_file = pathlib.Path(parameters['output']['projects']['feather file'])
-projects_data_types = parameters['data']['projects']['data types']
-
-# publications
-publications_relative_path = parameters['input']['NIH']['publications']['relative path']
-publications_n_table = parameters['input']['NIH']['publications']['table number']
-publications_feather_file = pathlib.Path(parameters['output']['publications']['feather file'])
-publications_data_types = parameters['data']['publications']['data types']
-
-# ====================
-
-re_javascript = re.compile('^javascript.*')
-
-relative_path_list = [publications_relative_path, projects_relative_path]
-n_table_list = [publications_n_table, projects_n_table]
-file_patterns_to_drop_list = [
-	[re_javascript, re.compile('.*_AFFLNK_.*\.zip$')],
-	[re_javascript, re.compile('.*_DUNS_.*\.zip$'), re.compile('.*_PRJFUNDING_.*\.zip$')]
-]
-feather_file_list = [publications_feather_file, projects_feather_file]
-dtype_list = [publications_data_types, projects_data_types]
-names_list = ['publications', 'projects']
-
 res = dict()
 
-for relative_path, n_table, file_patterns_to_drop, feather_file, dtype, name in zip(
-		relative_path_list, n_table_list, file_patterns_to_drop_list, feather_file_list, dtype_list, names_list):
+for p in [parameters['projects'], parameters['publications']]:
 
-	if not feather_file.exists():
+	if not pathlib.Path(p['feather file']).exists():
 
-		url = urllib.parse.urljoin(nih_homepage, relative_path)
+		url = urllib.parse.urljoin(nih_homepage, p['relative path'])
 
-		df = parser.parse_table(url, n_table, file_patterns_to_drop)
+		regular_expressions = [re.compile(expr) for expr in p['filename patterns to be ignored']]
 
-		unzipped_files_directory = downloads_directory / unzipped_files_directory / name
+		df = parser.parse_table(url, p['table number'], regular_expressions)
+
+		unzipped_files_directory = downloads_directory / unzipped_files_directory / p['name']
 
 		downloaded_files, uncompressed_files = download.files_list(
 			df['CSV_link'], downloads_directory, nih_homepage, df['CSV'], unzip_to=unzipped_files_directory)
 
-		df = util.dataframe_from_csv_files(uncompressed_files, dtype)
+		df = util.dataframe_from_csv_files(uncompressed_files, p['data types'])
 
-		df.to_feather(feather_file)
+		df.to_feather(p['feather file'])
 
 	else:
 
-		df = pd.read_feather(feather_file)
+		print(f'loading {p["feather file"]}...')
 
-	res[name] = df
+		df = pd.read_feather(p['feather file'])
+
+	res[p['name']] = df
 
 breakpoint()
 
